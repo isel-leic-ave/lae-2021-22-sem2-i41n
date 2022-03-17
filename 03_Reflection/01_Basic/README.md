c39d49cbfd36df146f879dad2b1aba93e83d19cf
João Trindade <joao.trindade@isel.pt>
2022-03-17 23:41:02 +0000

    Lecture 3b - Adjust code; add README

---
+++ 03_Reflection/01_Basic/README.md
@@ -0,0 +1,12 @@
+Compilar com:
+```
+   kotlinc prog.kt
+```
+
+Analisar:
+- estrutura de directorias correspondente ao nome do package declarado no código
+
+Executar:
+```
+   kotlin pt.isel.leic.lae.ProgKt
+```
--- 03_Reflection/01_Basic/prog.kt
+++ 03_Reflection/01_Basic/prog.kt
@@ -3,7 +3,6 @@ package pt.isel.leic.lae
 import kotlin.reflect.KClass
 
 class ClassWithProperties(number: Int, val txt: String, var flt: Double) {
-
 	val num = number + 1000
 	
 	fun mcwp() {
@@ -12,30 +11,36 @@ class ClassWithProperties(number: Int, val txt: String, var flt: Double) {
 }
 
 fun inspect(obj : Any) {
-	val kls = obj::class
+	println("--------")
 
-	println(kls.simpleName)
-	println(kls.qualifiedName)
+	// Get a KClass object from an instance
+	val kcls = obj::class
+	println("full  name: " + kcls.qualifiedName)
+	println("short name: " + kcls.simpleName)
 	
-	println("[")
-	kls.members.forEach {
-		mbr -> println("   " + mbr)
+	println("--------")
+	println("members: [")
+	kcls.members.forEach { mbr ->
+		println("   " + mbr)  // show all members of obj's type
 		if (mbr.name == "mcwp") {
-			mbr.call(obj)
+			print("      >>>   ")
+			mbr.call(obj)  // invokes method mcwp for the instance referred by obj
 		}
 	}
 	println("]")
 }
 
 fun main() {
+	println("--------")
 
-	val kls : KClass<ClassWithProperties> =
+	// Get a KClass object from the class
+	val kcls : KClass<ClassWithProperties> =
 		ClassWithProperties::class
 
-	println(kls.simpleName)
-	println(kls.qualifiedName)
+	println("full  name: " + kcls.qualifiedName)
+	println("short name: " + kcls.simpleName)
 
-	val cwp = ClassWithProperties(123, "ISEL", 3.141592654)
+	val obj = ClassWithProperties(10, "LAE", 1.2345)
 	
-	inspect(cwp)
+	inspect(obj)
 }
